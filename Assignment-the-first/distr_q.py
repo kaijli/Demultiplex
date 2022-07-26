bf03d2a17b97a66e1ff5337a942c49723cbeab11
kaijli <kaitlyn.j.li@gmail.com>
2022-07-26 12:01:15 -0700

    created files for assignment

---
+++ Assignment-the-first/distr_q.py
@@ -0,0 +1,8 @@
+
+import numpy as np
+import bioinfo as b
+
+read1 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz"
+index1 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz"
+index2 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz"
+read2 = "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz"
